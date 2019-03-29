0831e0a907eec91b6c324ac62fefd9e2cc9fba93
Malte Hübner <maltehuebner@gmx.org>
2019-03-29 12:48:03 +0100

    Init bundle.

---
+++ DependencyInjection/MalteHuebnerImpressExtension.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace MalteHuebner\ImpressBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class MalteHuebnerImpressExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $xmlLoader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $xmlLoader->load('services.xml');
+    }
+}
---
+++ MalteHuebnerImpressBundle.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace MalteHuebner\Impressbundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class MalteHuebnerImpressBundle extends Bundle
+{
+}
