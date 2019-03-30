ab0d4a1ca49f27c717914829171c9b91e15b661a
Malte Hübner <maltehuebner@gmx.org>
2019-03-30 22:06:20 +0100

    Introduce ImpressModel.

---
+++ Model/ImpressModel.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+namespace MalteHuebner\ImpressBundle\Model;
+
+class ImpressModel
+{
+    /** @var string $firstName */
+    protected $firstName;
+
+    /** @var string $lastName */
+    protected $lastName;
+
+    /** @var string $street */
+    protected $street;
+
+    /** @var string $houseNumber */
+    protected $houseNumber;
+
+    /** @var string $zipCode */
+    protected $zipCode;
+
+    /** @var string $city */
+    protected $city;
+
+    /** @var string $country */
+    protected $country;
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): ImpressModel
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): ImpressModel
+    {
+        $this->lastName = $lastName;
+        
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): ImpressModel
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getHouseNumber(): ?string
+    {
+        return $this->houseNumber;
+    }
+
+    public function setHouseNumber(string $houseNumber): ImpressModel
+    {
+        $this->houseNumber = $houseNumber;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(string $zipCode): ImpressModel
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): ImpressModel
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): ImpressModel
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
